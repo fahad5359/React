3f27f1b0deb9ac0913c0120db07bf21d4088a826
fahad5359 <fabbgm5359@gmail.com>
2022-05-11 05:15:22 +0300

    .

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import  React from 'react';
 import './App.css';
 import GoodBey from './components/GoodBey';
 import Welcome from './components/Welcome';
+import Addition from './components/Addition';
 
 
 function App() {
-  const  dawesome= "klb"
+  const  dawesome= "somthing"
   let aa=50
   let bb=50
   let  sum=aa+bb
@@ -18,13 +19,14 @@ function App() {
          <input type="submit"></input> 
         </p>
         <p>{dawesome}</p>
-        {/* we  can calculate values and using JSX we can bring these values on the HTML file*/}
+        {/* we  can calculate values and using JSX we can bring these values on the HTML file*/ }
         <p>What is the sum of 50 +  50 </p>
         <p>it is {sum}</p>
 
         <p>the Welcome component is bellow</p>
         <Welcome></Welcome>
         <GoodBey></GoodBey>
+        <Addition></Addition>
       
      
     </div>
--- src/components/addition.js
+++ src/components/addition.js
@@ -1,7 +1,12 @@
 import React from 'react'
 // add style to iddintyfy the  component,then do some addtion ,finally import it to app component
 export default function addition() {
+    let a=10;
+    let b=30
   return (
-    <div>addition</div>
+    <div className="stl">Again we made an addition that calculates a and b
+    <p>10+30={a+b}</p>
+    
+     </div>
   )
 }
